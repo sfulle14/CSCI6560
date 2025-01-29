d52eb6361be31c1ee1202e9f0c802a066d56e971
Steven Fuller <steven.fuller96@gmail.com>
2025-01-28 20:08:53 -0600

    created stored procedures to be called for pulling restricted data

--- index.php
+++ index.php
@@ -19,16 +19,30 @@
 
         <hr>
         
-        <div class="row">
-            <form action="insert_staff.php" method="POST">
-                <label for="UserName">UserName:</label>
-                <input type="text" id="UserName" name="UserName"><br><br>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form action="front/verify_login.php" method="POST" class="form">
+                    <div class="form-group">
+                        <label for="username">Username:</label>
+                        <input type="text" class="form-control" id="username" name="username" required>
+                    </div>
 
-                <label for="Password">Password:</label>
-                <input type="text" id="Password" name="Password"><br><br>
-            </form>
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
-
     </div>
+
+    <!-- Scripts used to help with Bootstrap formatting -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
