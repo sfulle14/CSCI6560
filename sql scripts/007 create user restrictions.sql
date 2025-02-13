de0b4b15ad4bcaff106ff861261fa303643e6621
Steven Fuller <steven.fuller96@gmail.com>
2025-02-13 16:12:23 -0600

    added template for user db rights

--- sql scripts/007 create user restrictions.sql
+++ sql scripts/007 create user restrictions.sql
@@ -1,5 +1,32 @@
+
+-- Admin user has total access
 CREATE USER 'dduck'@'localhost'
 IDENTIFIED BY 'password123';
 GRANT ALL ON Payroll.* TO 'dduck'@'localhost' WITH GRANT OPTION;
 
 SHOW GRANTS FOR 'B_Fuller'@'localhost';
+
+-- HR user
+CREATE USER 'mmouse'@'localhost'
+IDENTIFIED BY 'password123';
+GRAND ALL ON Payroll.Role TO 'mmouse'@'localhost' WITH GRANT OPTION;
+GRAND ALL ON Payroll.Employee TO 'mmouse'@'localhost' WITH GRANT OPTION;
+GRAND ALL ON Payroll.EmpLogin TO 'mmouse'@'localhost' WITH GRANT OPTION;
+GRAND ALL ON Payroll.PaymentHistory TO 'mmouse'@'localhost' WITH GRANT OPTION;
+GRAND ALL ON Payroll.Salary TO 'mmouse'@'localhost' WITH GRANT OPTION;
+
+
+-- Manager user
+CREATE USER 'minnie'@'localhost'
+IDENTIFIED BY 'password123';
+GRAND ALL ON Payroll.Employee TO 'minnie'@'localhost' WITH GRANT OPTION;
+GRAND ALL ON Payroll.PaymentHistory TO 'minnie'@'localhost' WITH GRANT OPTION;
+GRAND ALL ON Payroll.Salary TO 'minnie'@'localhost' WITH GRANT OPTION;
+
+
+-- Employee user
+CREATE USER 'pluto'@'localhost'
+IDENTIFIED BY 'password123';
+GRAND ALL ON Payroll.Employee TO 'pluto'@'localhost' WITH GRANT OPTION;
+GRAND ALL ON Payroll.PaymentHistory TO 'pluto'@'localhost' WITH GRANT OPTION;
+GRAND ALL ON Payroll.Salary TO 'pluto'@'localhost' WITH GRANT OPTION;
